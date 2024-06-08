acb2ea2ad7a13d5797a50b08c6a6bb9e54275d2c
Ganji Rajesh <99713423+ganji-rajesh@users.noreply.github.com>
2024-06-08 15:55:50 +0530

    Update dep_file.py

--- dep_file.py
+++ dep_file.py
@@ -1,1 +1,18 @@
+import streamlit as st
+from darts.models import ExponentialSmoothing
+
+st.write('deploying_1st_model')
+
+@st.cache_resource
+def load_model():
+    model = ExponentialSmoothing.load('hwes_m_model.pkl')
+    return model
+
+with st.spinner('loading model'):
+    model = load_model()
+
+st.write('prediction')
+with st.spinner('predicting'):
+    pred = model.predict(12)
+    st.write(pred)
 
